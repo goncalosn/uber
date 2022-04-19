d0ee8d43b767110faf25a3b479d3f13b576fd9db
Gonçalo Nunes <goncalonunes1111@gmail.com>
2022-04-19 22:54:44 +0100

    feat: test dev cicd for site

--- packages/site/src/App/index.tsx
+++ packages/site/src/App/index.tsx
@@ -13,6 +13,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <div>
           <p>Hello Vite + React!</p>
+          <br />
           <HelloWorld />
         </div>
         <p>
