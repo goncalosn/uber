b031608b401339cc039311b90f55db1ce2bb9612
Gonçalo Nunes <goncalonunes1111@gmail.com>
2022-04-22 02:32:52 +0100

    fix: prod deployment for site

--- packages/site/src/App/index.tsx
+++ packages/site/src/App/index.tsx
@@ -13,7 +13,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <div>
           <p>Hello Vite + React!</p>
-          <HelloWorld /> 1
+          <HelloWorld /> 2
         </div>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
